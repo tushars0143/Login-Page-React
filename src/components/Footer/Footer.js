af950887d7cc51a810c2375cadd402954c778057
Tushar Soni <100558471+tushars0143@users.noreply.github.com>
2022-05-22 12:29:38 -0700

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>Copyright © 2022 Tushar Soni, Inc. All Rights Reserved</footer>
+    )
+  }
+}
+
+export default Footer;
